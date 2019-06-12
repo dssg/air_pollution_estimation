b19d8d35dab411d9f8b3fc58ff9f8b8acc0bf255
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-06-12 10:41:30 +0100

    Upload tims data in chunks and keep track of already uploaded data.

--- src/d01_data/collect_tims_data.py
+++ src/d01_data/collect_tims_data.py
@@ -41,4 +41,60 @@ def get_tims_data_and_upload_to_s3(local_tims_dir: str,
                            local_tims_dir
                            ])
 
+def get_tims_data_and_upload_to_s3_in_chunk(local_tims_dir: str,
+                                   file_website: str = 'https://s3-eu-west-1.amazonaws.com/roads.data.tfl.gov.uk',
+                                   download_website: str = "http://roads.data.tfl.gov.uk/TIMS/",
+                                   uploaded_file: str = "uploaded_file.txt",
+                                   chunk_size = 5
+                                   ):
+    # get html file from website
+    r = requests.get(file_website)
+    data = r.text
+    soup = BeautifulSoup(data, features="html.parser")
+
+    # extract the list of available file names
+    keys = list(soup.find_all('key'))
+    keys = [str(key) for key in keys]
+    files = [key.replace('<key>', '').replace('</key>', '').replace('TIMS/', '') for key in keys
+             if 'TIMS' in key and '.csv' in key]
+
+    # create tims directory if it doesn't exist
+    if not os.path.exists(local_tims_dir):
+        os.makedirs(local_tims_dir)
+
+
+    if not os.path.exists(uploaded_file):
+        uploaded_files_list = []
+    else:
+        with open(uploaded_file, 'r') as f:
+            uploaded_files_list = f.readlines()
+
+    files = [filename for filename in  files if filename not in uploaded_files_list]
+    while files:
+        # delete local tims directory
+        res = subprocess.call(["rm", "-r",
+                        local_tims_dir
+                        ])
+
+        # create tims directory if it doesn't exist
+        if not os.path.exists(local_tims_dir):
+            os.makedirs(local_tims_dir)
+        
+        chunk = files[:chunk_size]
+        resp = [urllib.request.urlretrieve( download_website + filename, local_tims_dir + "/" + filename) for filename in chunk]
+
+        res = subprocess.call(["aws", "s3", 'cp',
+                                local_tims_dir,
+                                's3://air-pollution-uk/raw/tims_data/',
+                                '--recursive',
+                                '--profile',
+                                'dssg'])
+
+        with open(uploaded_file, "a") as f:
+            print("\n".join(chunk), file=f)
+            print("Saved ", chunk)
+
+        files = files[chunk_size:]
+
+
     
---
+++ src/d01_data/upload_tims_main.py
@@ -0,0 +1,32 @@
+import collect_tims_data
+from collect_video_data import collect_video_data
+from collect_tims_data import get_tims_data_and_upload_to_s3_in_chunk
+import os
+import sys
+import configparser
+import ast
+
+if __name__ == "__main__":
+    setup_dir = os.path.join(os.getcwd(),'.')
+    src_dir = os.path.join(setup_dir, 'src')
+    sys.path.append(src_dir)
+    print(os.path.join(setup_dir, 'conf', 'base', 'parameters.yml'))
+
+    # credentials
+    config = configparser.ConfigParser()
+    config.read(os.path.join(setup_dir, 'conf', 'local', 'credentials.yml'))
+    config.read(os.path.join(setup_dir, 'conf', 'base', 'parameters.yml'))
+
+    # local data folder
+    video_dir = os.path.join(setup_dir, 'data', '01_raw', 'video_data')
+    tims_dir = os.path.join(setup_dir, 'data', '01_raw', 'tims')
+
+    uploaded_file = os.path.join(setup_dir, 'data', '01_raw', 'uploaded_file.txt')
+
+    
+    # tims data
+    get_tims_data_and_upload_to_s3_in_chunk(local_tims_dir = tims_dir,
+                              file_website = ast.literal_eval(config.get('DATA_COLLECTION', 'tims_file_website')),
+                              download_website = ast.literal_eval(config.get('DATA_COLLECTION', 'tims_download_website')), 
+                              uploaded_file = uploaded_file)    
+    
