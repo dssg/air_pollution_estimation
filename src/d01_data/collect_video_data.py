eee5492044c7637e80190e5ab9307695d2aa4c01
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-06-13 11:37:45 +0100

    Check the available property in the  video data json.
    This function might deprecated in the future because the available field does not actually show the status of the camera.

--- src/d01_data/collect_video_data.py
+++ src/d01_data/collect_video_data.py
@@ -79,7 +79,6 @@ def collect_video_data(local_video_dir: str,
                                                       num_iterations))
             time.sleep(3 * 60)
 
-
 def collect_camera_videos(local_video_dir:str,
     num_iterations:int = None,
     website:str = "https://api.tfl.gov.uk/Place/Type/JamCam",
@@ -125,10 +124,58 @@ def collect_camera_videos(local_video_dir:str,
 
                 with open(cam_file, 'w') as f:
                     json.dump(new_video_urls, f)
+
+def collect_available_camera_videos(local_video_dir:str,
+    num_iterations:int = None,
+    website:str = "https://api.tfl.gov.uk/Place/Type/JamCam",
+    cam_file:str="cam_file.json"):
+
+    # check if api is working
+    if not os.path.exists(local_video_dir):
+        os.makedirs(local_video_dir)
+
+    res = urllib.request.urlopen(website)
+    data = json.loads(res.read())
+    
+    new_video_urls = defaultdict()
+
+    if not os.path.exists(cam_file):
+        video_urls_dict = defaultdict(str)
+    else:
+        with open(cam_file, 'r') as f:
+            video_urls_dict = dict(json.loads(f.read()))
+
+    # parse data
+    for item in data:
+        additionalProperties = item['additionalProperties']
+        properties = {val["key"]:val for val in additionalProperties}
+
+        available_prop = properties["available"]
+
+        if available_prop["value"] == "true":
+            video_prop = properties["videoUrl"]
+
+            video_url = video_prop['value']
+            filename = video_url.split('/')[-1]
+            
+            timestamp = video_prop['modified']
+            file_path = os.path.join(local_video_dir, timestamp+"_"+filename)
+
+            print("Checking if video already exist")
+
+            # check if video already exist
+            if filename in video_urls_dict and video_urls_dict[filename] == timestamp:
+                print("Video already exist")
+                continue
+
+            # download video
+            print("Downloading videos to ", file_path)
+            urllib.request.urlretrieve(prop['value'], file_path)
+            new_video_urls[filename] = prop['modified']
+
+            with open(cam_file, 'w') as f:
+                json.dump(new_video_urls, f)
         
- 
-
-
 
 
 
