871f6df9894ecb3b1c2a791374c6c850a1595719
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-06-13 11:45:09 +0100

    Added a file to track the cameras that have been modified.

---
+++ src/d01_data/download_video_data_main.py
@@ -0,0 +1,24 @@
+import collect_tims_data
+from collect_video_data import collect_video_data, collect_camera_videos
+from collect_tims_data import get_tims_data_and_upload_to_s3
+import os
+import sys
+import configparser
+import ast
+import time
+
+
+if __name__ == "__main__":
+    setup_dir = os.path.join(os.getcwd(),'.')
+    
+    print(os.path.join(setup_dir, 'conf', 'base', 'parameters.yml'))
+    
+    # local data folder
+    video_dir = os.path.join(setup_dir, 'data', '01_raw', 'video_data')
+    tims_dir = os.path.join(setup_dir, 'data', '01_raw', 'tims')
+
+    uploaded_file = os.path.join(setup_dir, 'data', '01_raw', 'uploaded_file.json')
+
+    while True:
+        collect_camera_videos(local_video_dir=video_dir)
+        time.sleep(2 * 60)
