48b3f43bcea956af3b85222c6bdd87b15e21e8eb
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-06-13 16:53:36 +0100

    Removed wrong method from import

--- src/d01_data/upload_video_data_main.py
+++ src/d01_data/upload_video_data_main.py
@@ -1,5 +1,5 @@
 import collect_tims_data
-from collect_video_data import collect_video_data, collect_camera_videos, upload_videos
+from collect_video_data import collect_camera_videos, upload_videos
 from collect_tims_data import get_tims_data_and_upload_to_s3
 import os
 import sys
@@ -20,9 +20,7 @@ if __name__ == "__main__":
     # local data folder
     video_dir = os.path.join(setup_dir, 'data', '01_raw', 'video_data')
     tims_dir = os.path.join(setup_dir, 'data', '01_raw', 'tims')
-
     uploaded_file = os.path.join(setup_dir, 'data', '01_raw', 'uploaded_file.json')
-
     while True:
         upload_videos(local_video_dir=video_dir)
         time.sleep(2 * 60)
