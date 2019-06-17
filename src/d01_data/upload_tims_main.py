b9df7b1a6870052066d46ff265cc181714b47670
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-06-17 19:14:15 +0100

    Updated relative path.

--- src/d01_data/upload_tims_main.py
+++ src/d01_data/upload_tims_main.py
@@ -5,7 +5,7 @@ import configparser
 import ast
 
 if __name__ == "__main__":
-    setup_dir = os.path.join(os.getcwd(), '.')
+    setup_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
 
     # credentials
     config = configparser.ConfigParser()
@@ -13,8 +13,8 @@ if __name__ == "__main__":
 
     # local data folder
     tims_dir = os.path.join(setup_dir, 'data', '01_raw', 'tims')
-    uploaded_file = os.path.join(
-        setup_dir, 'data', '01_raw', 'uploaded_file.txt')
+    uploaded_tims_data_file = os.path.join(
+        setup_dir, 'data', '00_ref', 'uploaded_tims_data_file.txt')
 
     # tims data
     get_tims_data_and_upload_to_s3(local_tims_dir=tims_dir,
@@ -22,4 +22,5 @@ if __name__ == "__main__":
                                                 'DATA_COLLECTION', 'tims_file_website')),
                                             download_website=ast.literal_eval(config.get(
                                                 'DATA_COLLECTION', 'tims_download_website')),
-                                            uploaded_file=uploaded_file)
+                                                uploaded_tims_data_file=uploaded_tims_data_file
+                                            )
