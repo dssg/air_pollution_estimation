7a2be4d9ddb9e5cc6c72917b09dd30e8c7733026
Sam Blakeman <sam.blakeman03@gmail.com>
2019-06-20 18:10:14 +0100

    Merging of Mine and Jacks functions

--- src/d00_utils/data_retrieval.py
+++ src/d00_utils/data_retrieval.py
@@ -8,75 +8,118 @@ import shutil
 import datetime
 
 
-def retrieve_videos_from_s3(bool_keep_data=True, from_date=None, to_date=None):
+def retrieve_single_video(camera, date, time, bool_keep_data=True):
+    """Retrieve one jamcam video from the s3 bucket based on the details specified.
+        Downloads to a local directory and then loads into a numpy array.
+
+            Args:
+                camera: camera number as a string
+                date: date as a string (YYYYMMDD)
+                time: time as a string (HHMM)
+                bool_keep_data: boolean for keeping the downloaded data in the local folder
+            Returns:
+                numpy array containing the jamcam video
+            Raises:
+
+        """
+    local_dir = create_local_dir()
+    timestamp = date[:4] + "-" + date[4:6] + "-" + date[6:] + "_" + time[:2] + '.' + time[2:]
+    s3_vid_key = "raw" + "/" + "video_data" + "/" + camera + "/" + timestamp + '.mp4'
+    s3_bucket = connect_to_bucket()
+
+    file_dir = local_dir + date + "_" + time + "_" + camera + '.mp4'
+    s3_bucket.download_file(s3_vid_key, file_dir)
+    buf = mp4_to_npy(file_dir)
+
+    if (not bool_keep_data):
+        shutil.rmtree(local_dir)
+
+    return buf
+
+
+def retrieve_videos_based_on_dates(from_date='2019-06-01', to_date=str(datetime.datetime.now())[:10], bool_keep_data=True):
     """Retrieve jamcam videos from the s3 bucket based on the dates specified.
     Downloads to a local directory and then loads them into numpy arrays.
 
         Args:
-            bool_keep_data: boolean for keeping the downloaded data in the local folder
             from_date: start date (inclusive) for retrieving videos, if None then will retrieve from 2019-06-01 onwards
             to_date: end date (inclusive) for retrieving vidoes, if None then will retrieve up to current day
+            bool_keep_data: boolean for keeping the downloaded data in the local folder
         Returns:
             list of numpy arrays containing all the jamcam videos between the selected dates
         Raises:
 
     """
-
-    # Deal with the cases when a date isn't specified
-    if (from_date == None):
-        from_date = '2019-06-01'
-    if (to_date == None):
-        to_date = str(datetime.datetime.now())[:10]
-
-    # Set local directory for downloading data, will overwrite whatever is currently there
-    local_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             '..', '..', 'data/01_raw/jamcams/')
-    if (os.path.isdir(local_dir)):
-        shutil.rmtree(local_dir)
-    os.mkdir(local_dir)
-
-    # Set up boto3 session
-    s3_session = boto3.Session(profile_name='dssg')
-    bucket_name = 'air-pollution-uk'
-    s3_resource = s3_session.resource('s3')
-    my_bucket = s3_resource.Bucket(bucket_name)
+    local_dir = create_local_dir()
+    my_bucket = connect_to_bucket()
 
     # Get list of files in s3 based on dates provided
     selected_files = []
     objects = my_bucket.objects.filter(Prefix="raw/video_data_new/")
-    files = [obj.key for obj in objects]
-    for file in files:
-        date = re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2})", file).group()
-        if (date >= from_date and date <= to_date):
-            selected_files.append(file)
+
+    for obj in objects:
+        file = obj.key
+        try:
+            date = re.search("([0-9]{4}\-[0-9]{2}\-[0-9]{2})", file).group()
+            if (date >= from_date and date <= to_date):
+                selected_files.append(file)
+        except:
+            print('Could not find date for: ' + file)
 
     # Download the selected files
-    data = []
     for file in selected_files:
         my_bucket.download_file(file, local_dir + file.split('/')[-1])
 
     # Load files into a list of numpy arrays using opencv
+    data = []
     for file in os.listdir(local_dir):
-        cap = cv2.VideoCapture(local_dir + file)
-
-        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        buf = np.empty((frame_count, frame_height, frame_width, 3), np.dtype('uint8'))
-        fc = 0
-        ret = True
-        while (fc < frame_count and ret):
-            ret, buf[fc] = cap.read()
-            fc += 1
-        cap.release()
-        data.append(buf)
+        data.append(mp4_to_npy(local_dir + file))
 
     # Delete local data unless specified
     if(not bool_keep_data):
         shutil.rmtree(local_dir)
 
     return data
+
+
+def create_local_dir():
+    """Creates the local directory for downloading from s3"""
+    # Set local directory for downloading data, will overwrite whatever is currently there
+    local_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             '..', '..', 'data/01_raw/jamcams/')
+    if (os.path.isdir(local_dir)):
+        shutil.rmtree(local_dir)
+    os.mkdir(local_dir)
+    return local_dir
+
+
+def mp4_to_npy(local_mp4_path):
+    """Load mp4 file from the local directory and turn it into a numpy array"""
+    cap = cv2.VideoCapture(local_mp4_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    buf = np.empty((frame_count, frame_height, frame_width, 3), np.dtype('uint8'))
+    fc = 0
+    ret = True
+    while (fc < frame_count and ret):
+        ret, buf[fc] = cap.read()
+        fc += 1
+    cap.release()
+
+    return buf
+
+
+def connect_to_bucket():
+    """Connects to the s3 bucket"""
+    # Set up boto3 session
+    s3_session = boto3.Session(profile_name='dssg')
+    bucket_name = 'air-pollution-uk'
+    s3_resource = s3_session.resource('s3')
+    my_bucket = s3_resource.Bucket(bucket_name)
+
+    return my_bucket
 
 
 def retrieve_tims_from_s3():
@@ -87,7 +130,6 @@ def retrieve_tims_from_s3():
 
 
 def retrieve_cam_details_from_database():
-
 
 
 
@@ -105,12 +147,7 @@ def describe_s3_bucket():
             Raises:
 
         """
-
-    # Set up boto3 session
-    s3_session = boto3.Session(profile_name='dssg')
-    bucket_name = 'air-pollution-uk'
-    s3_resource = s3_session.resource('s3')
-    my_bucket = s3_resource.Bucket(bucket_name)
+    my_bucket = connect_to_bucket()
 
     # Get list of all dates in the s3 bucket
     objects = my_bucket.objects.filter(Prefix="raw/video_data_new/")
@@ -129,7 +166,7 @@ def describe_s3_bucket():
     plt.ylabel('Number of Videos')
     plt.tight_layout()
     plt.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                      '..', '..', 'plots/01_exploratory/s3_description.pdf'))
+                             '..', '..', 'plots/01_exploratory/s3_description.pdf'))
     plt.close()
 
     return
