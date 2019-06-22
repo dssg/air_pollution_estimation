ad3931664c34529c74ab4a7db3d07bdaeb591e47
Sam Blakeman <sam.blakeman03@gmail.com>
2019-06-22 13:53:16 +0100

    Finishing off parsing of xml file

--- src/d04_modelling/Evaluation.py
+++ src/d04_modelling/Evaluation.py
@@ -2,21 +2,22 @@ import numpy as np
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-paths = {'annotations': '../../annotations/'}
 
-def evaluate_yolo_predictions(videos, yolo_output, paths):
+def parse_annotations(xml_files, paths, bool_print_summary=False):
+    """ Parse the XML files containing the manual annotations
+                Args:
+                    xml_files: list of the XML files to parse
+                    paths: dictionary of paths (needs 'annotations' path)
+                    bool_print_summary: boolean for printing summaries of each parsed XML file
+                Returns:
+                    pandas dataframe containing the annotations
+                Raises:
+    """
 
-    for video in videos:
-        # load the annotated xml file
-        root = ET.parse(paths['annotations'] + video + '.xml').getroot()
-
+    for xml_file in xml_files:
+        root = ET.parse(paths['annotations'] + xml_file + '.xml').getroot()
         annotated_results = {'id': [], 'frame': [], 'occluded': [], 'bounds': [],
                              'type': [], 'parked': [], 'stopped': []}
-
-        num_vehicles = 0
-        num_parked = 0
-        num_stops = 0
-        types = []
 
         for track in root.iter('track'):
             if(track.attrib['label'] == 'vehicle'):
@@ -31,18 +32,40 @@ def evaluate_yolo_predictions(videos, yolo_output, paths):
 
         df = pd.DataFrame.from_dict(annotated_results)
 
-        print('Number of vehicles:')
-        print(df.groupby('type')['id'].nunique())
+        if(bool_print_summary):
+            print('Number of vehicles:')
+            print(df.groupby('type')['id'].nunique())
+            print('Parked vehicles:')
+            print(df.groupby('id')['parked'].unique())
+            stops_df = get_stop_counts(df)
+            print('Number of Stops:')
+            print(stops_df)
 
-        print('Parked vehicles:')
-        print(df.groupby('id')['parked'].unique())
-
-        print('Number of stops: ' + str(num_stops))
-        vals, counts = np.unique(types, return_counts=True)
-        print('Vehicle Types: ' + str(vals))
-        print('Type Counts: ' + str(counts))
-
-    return
+    return df
 
 
-evaluate_yolo_predictions(['video001'], None, paths)
+def get_stop_counts(annotations_df):
+    """ Get the number of stops for each vehicle
+                    Args:
+                        annotations_df: pandas dataframe containing the annotations
+                    Returns:
+                        pandas dataframe containing the vehicle ids and the number of stops
+                    Raises:
+    """
+    ids, counts = [], []
+    df_grouped = annotations_df.sort_values(['frame'], ascending=True).groupby('id')
+    for group in df_grouped:
+        bool_stopped = False
+        num_stops = 0
+        for val in group[1]['stopped']:
+            if (val == 'true' and not bool_stopped):
+                bool_stopped = True
+            elif (val == 'false' and bool_stopped):
+                num_stops += 1
+                bool_stopped = False
+        if (bool_stopped):
+            num_stops += 1
+        ids.append(group[1]['id'].tolist()[0])
+        counts.append(num_stops)
+    stops_df = pd.DataFrame(data=np.array(list(zip(ids, counts))), columns=['id', 'num_stops'])
+    return stops_df
