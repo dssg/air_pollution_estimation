c821b6c488045b25d9535c0bfa40755143be084a
Sam Blakeman <sam.blakeman03@gmail.com>
2019-06-26 10:46:38 +0100

    basic pipeline

--- src/d04_modelling/classify_objects.py
+++ src/d04_modelling/classify_objects.py
@@ -1,18 +1,18 @@
-import cv2
 import numpy as np
 from cvlib.object_detection import draw_bbox
 import cvlib as cv
 import imageio
-import time
-import os
 
 
-def classify_objects(local_mp4_path, params, vid_time_length=10, make_video=True):
+def classify_objects(video, params, paths, vid_time_length=10, make_video=True, vid_name='unnamed'):
     """ this function classifies objects from local mp4 with cvlib python package.
         Args:
-            local_mp4_path (str): path to mp4 file in directory
+            video (nparray): numpy array containing the video
+            params (dict): dictionary of parameters from yml file
+            paths (dict): dictionary of paths from yml file
             vid_time_length (int): length of the video data in seconds
             make_video (bool): output a video with object classification labels in same directory as original video
+            vid_name (str): name of the output video
 
         Returns:
             obj_bounds (list of np arrays): n-dim list of list of arrays marking the corners of the bounding boxes of objects, for n frames
@@ -20,24 +20,14 @@ def classify_objects(local_mp4_path, params, vid_time_length=10, make_video=True
             obj_label_confidences (list of floats): n-dim list of list of floats denoting yolo confidences, for n frames
     """
 
-    start_time = time.time()
-
-    # import video from local path
-    cap = cv2.VideoCapture(local_mp4_path)
-    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap_fps = int(n_frames / vid_time_length)  # assumes vid_length in seconds
-
     # loop over frames of video and store in lists
     obj_bounds = []
     obj_labels = []
     obj_label_confidences = []
     cap_cvlib = []
 
-    while cap.isOpened():
-        # open imported video
-        status, frame = cap.read()
-        if not status:
-            break
+    for i in range(video.shape[0]):
+        frame = video[i, :, :, :]
 
         # apply object detection
         bbox, label, conf = cv.detect_common_objects(frame, confidence=params['confidence_threshold'],
@@ -50,16 +40,11 @@ def classify_objects(local_mp4_path, params, vid_time_length=10, make_video=True
         if make_video:
             img_cvlib = draw_bbox(frame, bbox, label, conf)
             cap_cvlib.append(img_cvlib)
-        else:
-            pass
 
     # write video to local file
     if make_video:
         cap_cvlib_npy = np.asarray(cap_cvlib)
-        local_mp4_path_out = local_mp4_path[:-4] + '_cvlib' + local_mp4_path[-4:]
-        imageio.mimwrite(local_mp4_path_out, cap_cvlib_npy, fps=cap_fps)
-    else:
-        pass
+        local_mp4_path_out = paths['local_video'] + vid_name + '.mp4'
+        imageio.mimwrite(local_mp4_path_out, cap_cvlib_npy, fps=int(video.shape[0] / vid_time_length))
 
-    #print('Run time is %s seconds' % (time.time() - start_time))
     return obj_bounds, obj_labels, obj_label_confidences
--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,5 +1,5 @@
 
-from src.d00_utils.data_retrieval import describe_s3_bucket, retrieve_single_video
+from src.d00_utils.data_retrieval import retrieve_single_video
 from src.d00_utils.load_confs import load_parameters, load_paths
 
 from src.d04_modelling.classify_objects import classify_objects
@@ -7,13 +7,13 @@ from src.d04_modelling.classify_objects import classify_objects
 params = load_parameters()
 paths = load_paths()
 
-#describe_s3_bucket(paths)
 camera = '03675'
 date = '2019-06-20'
 time = '13:25:31.321785'
 video = retrieve_single_video(camera, date, time, paths, bool_keep_data=True)
-local_mp4_path = paths['local_video'] + date + "_" + time + "_" + camera + '.mp4'
-obj_bounds, obj_labels, obj_label_confidences = classify_objects(local_mp4_path, params, vid_time_length=10, make_video=True)
+obj_bounds, obj_labels, obj_label_confidences = classify_objects(video, params, paths,
+                                                                 vid_time_length=10, make_video=False,
+                                                                 vid_name='Test')
 print(obj_bounds)
 print(obj_labels)
 print(obj_label_confidences)
