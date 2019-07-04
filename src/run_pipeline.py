60cb5d2c7848d12d2608b90b709a6ae75d388fe6
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-07-04 13:50:21 +0100

    Process videos in chunks

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,28 +1,24 @@
 import pandas as pd
 
-from src.d00_utils.data_retrieval import retrieve_videos_s3_to_np, load_videos_from_local, retrieve_video_names_from_s3, append_to_csv,load_existing_video_names, download_video_and_convert_to_numpy,delete_and_recreate_dir
+from src.d00_utils.data_retrieval import retrieve_videos_s3_to_np, load_videos_from_local, retrieve_video_names_from_s3, append_to_csv, load_video_names, download_video_and_convert_to_numpy, delete_and_recreate_dir
 from src.d00_utils.load_confs import load_parameters, load_paths
 
 from src.d04_modelling.classify_objects import classify_objects
 from src.d05_reporting.report_yolo import yolo_output_df, yolo_report_stats
-import os 
+import os
 
 params = load_parameters()
 paths = load_paths()
 
-# videos, names = retrieve_videos_s3_to_np(paths, from_date='2019-06-30', to_date='2019-06-30',
-#                                          from_time='13-00-00', to_time='13-05-00',
-#                                          camera_list=params['tims_camera_list'],
-#                                          bool_keep_data=True)
-
-print("Retrieving video filenames")
-selected_videos = retrieve_video_names_from_s3(
+print("Saving video filenames")
+retrieve_video_names_from_s3(
     paths, from_date='2019-06-30', to_date='2019-06-30',
-    from_time='13-00-00', to_time='13-04-00',
+    from_time='13-00-00', to_time='13-05-00',
     camera_list=params['tims_camera_list'],
     save_to_file=True)
-# selected_videos = load_existing_video_names(paths)
-print("Finished retrieving %s video filenames "%(len(selected_videos)))
+
+selected_videos = load_video_names(paths)
+print("Finished saving %s video filenames " % (len(selected_videos)))
 
 # select chunks of videos and classify objects
 while selected_videos:
@@ -36,8 +32,9 @@ while selected_videos:
     print("Classifying objects in video files")
     # classify objects in videos
     yolo_dict = classify_objects(videos, names, params, paths,
-        vid_time_length=10, make_videos=True)
+                                 vid_time_length=10, make_videos=False)
     yolo_df = yolo_output_df(yolo_dict)
+    print(yolo_df.head())
     stats_df = yolo_report_stats(yolo_df)
     print("Generated stats for some video files")
 
