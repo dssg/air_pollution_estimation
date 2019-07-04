20909c9c0025c3af2d8a1189188c877a302db2c2
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-04 19:31:02 +0100

    comments

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -7,16 +7,29 @@ from traffic_estimation.d05_reporting.report_yolo import yolo_output_df, yolo_re
 params = load_parameters()
 paths = load_paths()
 
+
+### select data for model validation runs
+
 videos, names = retrieve_videos_s3_to_np(paths, from_date='2019-06-30', to_date='2019-06-30',
                                          from_time='13-00-00', to_time='13-05-00',
                                          camera_list=params['tims_camera_list'],
                                          bool_keep_data=True)
 
+##### put boxes around objects at frame level
+
 #videos, names = load_videos_from_local(paths)
 yolo_dict = classify_objects(videos, names, params, paths,
                                 vid_time_length=10, make_videos=True)
 yolo_df = yolo_output_df(yolo_dict)
+
+###### count cars per video
+
 stats_df = yolo_report_stats(yolo_df)
 stats_df.to_csv(paths['processed_video'] + 'JamCamStats.csv')
+
+
+
+##### summarise model performance
+
 
 
