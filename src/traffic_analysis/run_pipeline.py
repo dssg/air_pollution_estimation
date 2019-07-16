fe56278d51f7981610dd9b4ded988a27d7062caf
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-07-16 16:42:06 +0100

    Formatted code.

--- src/traffic_analysis/run_pipeline.py
+++ src/traffic_analysis/run_pipeline.py
@@ -6,10 +6,10 @@ import os
 
 params = load_parameters()
 paths = load_paths()
-print(params['dtype'])
+
 print("Saving video filenames")
 retrieve_video_names_from_s3(
-    paths, from_date='2019-06-30', to_date='2019-07-10',
+    paths, from_date='2019-06-05', to_date='2019-06-18',
     from_time='00-00-00', to_time='23-59-59',
     camera_list=params['tims_camera_list'],
     save_to_file=True)
@@ -24,21 +24,21 @@ while selected_videos:
 
     # download chunks of videos
     videos, names = download_video_and_convert_to_numpy(
-        paths['temp_video'], paths['s3_profile'], paths['bucket_name'], selected_videos[:chunk_size])
+        local_folder=paths['temp_video'], s3_profile=paths['s3_profile'], bucket=paths['bucket_name'], filenames=selected_videos[:chunk_size])
     print("Finished downloading video files")
 
     print("Classifying objects in video files")
     # classify objects in videos
-    yolo_dict = classify_objects(videos, names, params, paths,
-                                 vid_time_length=10, make_videos=False)
+    yolo_dict = classify_objects(
+        videos=videos, names=names, params=params, paths=paths, vid_time_length=10, make_videos=False)
     yolo_df = yolo_output_df(yolo_dict)
     print(yolo_df.head())
     stats_df = yolo_report_stats(yolo_df)
     print("Generated stats for some video files")
 
     # append to csv
-    filepath = os.path.join(paths['processed_video'], 'JamCamStats.csv')
-    append_to_csv(filepath, stats_df, params['columns'], params['dtype'])
+    filename = os.path.join(paths['processed_video'], 'JamCamStats.csv')
+    append_to_csv(filename, stats_df, params['columns'], params['dtype'])
     print("Appended stats to big csv")
     selected_videos = selected_videos[5:]
     delete_and_recreate_dir(paths["temp_video"])
