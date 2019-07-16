883a5d0a9681e04415c0fcdd369e8bfc5f1ac3f0
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-16 18:14:14 +0100

    creating temp directory

--- src/traffic_analysis/d03_processing/update_s3_processed.py
+++ src/traffic_analysis/d03_processing/update_s3_processed.py
@@ -21,6 +21,7 @@ def update_frame_level_table(file_names, paths, params):
     """
     my_bucket = connect_to_bucket(paths['s3_profile'], paths['bucket_name'])
 
+    delete_and_recreate_dir(paths["temp_video"])
     # Download the video file_names using the file list
     for file in file_names:
         try:
