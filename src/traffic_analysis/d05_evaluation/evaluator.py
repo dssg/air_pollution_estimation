cad1dc6c83bd2910e2d4316657f319ed64aad871
Caroline <carolinewang01@gmail.com>
2019-07-17 15:01:07 +0100

    evaluator created

---
+++ src/traffic_analysis/d05_evaluation/evaluator.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+#create evaluator class(xml files, video level table, frame level table)
+#subclasses frame level and video level
+#evaluate_all functiion will evaluate frame level and video level by default 
+#to do this it will call frame_level_evaluation and video_level_evaluation functions
+#will do plotting as well
