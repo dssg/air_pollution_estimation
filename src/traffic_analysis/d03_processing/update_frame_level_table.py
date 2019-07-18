5763c20f2be0764ca0ec096d678da920d898f404
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-18 09:41:23 +0100

    broke files up and renamed

---
+++ src/traffic_analysis/d03_processing/update_frame_level_table.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import sqlalchemy
+
+from traffic_analysis.d00_utils.data_retrieval import connect_to_bucket, load_videos_into_np, delete_and_recreate_dir
+from traffic_analysis.d04_modelling.classify_objects import classify_objects
+from traffic_analysis.d03_processing.add_to_frame_table_sql import add_to_frame_table_sql
+
+
+def update_frame_level_table(file_names, paths, params, creds):
+    """ Update the frame level table on s3 (pq) based on the videos in the files list
+                Args:
+                    file_names (list): list of s3 filepaths for the videos to be processed
+                    paths (dict): dictionary of paths from yml file
+                    params (dict): dictionary of parameters from yml file
+
+                Returns:
+
+    """
+    my_bucket = connect_to_bucket(paths['s3_profile'], paths['bucket_name'])
+
+    delete_and_recreate_dir(paths["temp_video"])
+    # Download the video file_names using the file list
+    for file in file_names:
+        try:
+            my_bucket.download_file(file, paths["temp_video"] + file.split('/')[-1].replace(
+                ':', '-').replace(" ", "_"))
+        except:
+            print("Could not download " + file)
+
+    video_dict = load_videos_into_np(paths["temp_video"])
+    delete_and_recreate_dir(paths["temp_video"])
+
+    frame_level_df = classify_objects(video_dict=video_dict,
+                                      params=params,
+                                      paths=paths,
+                                      vid_time_length=10,
+                                      make_videos=False)
+
+    add_to_frame_table_sql(df=frame_level_df,
+                           table='frame_level',
+                           creds=creds)
+
+    return
+
+
+
+
