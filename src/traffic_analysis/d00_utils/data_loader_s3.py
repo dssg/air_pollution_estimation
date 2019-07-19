3e967cee50e96abff7de9a467178c2ec96f29217
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-19 17:51:54 +0100

    added file exists check

--- src/traffic_analysis/d00_utils/data_loader_s3.py
+++ src/traffic_analysis/d00_utils/data_loader_s3.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
 
 
 class DataLoaderS3:
@@ -7,6 +8,7 @@ class DataLoaderS3:
     def __init__(self,
                  s3_credentials: dict,
                  bucket_name: str):
+
         self.s3_credentials = s3_credentials
         self.bucket_name = bucket_name
 
@@ -17,12 +19,27 @@ class DataLoaderS3:
         self.client = client
 
     def read_json(self, file_path):
+
         result = self.client.get_object(Bucket=self.bucket_name,
                                         Key=file_path)
 
         return result['Body'].read().decode()
 
     def save_json(self, data, file_path):
+
         self.client.put_object(Body=(bytes(json.dumps(data).encode('UTF-8'))),
                                Bucket=self.bucket_name,
                                Key=file_path)
+
+    def file_exists(self, file_path):
+
+        try:
+            self.client.get_object(Bucket=self.bucket_name,
+                                   Key=file_path)
+            return True
+
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'NoSuchKey':
+                return False
+            else:
+                raise ex
