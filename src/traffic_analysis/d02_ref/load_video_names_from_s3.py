f4291829d44173b6ccdae5c6059813834f129189
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-22 11:42:31 +0300

    remove src from import

--- src/traffic_analysis/d02_ref/load_video_names_from_s3.py
+++ src/traffic_analysis/d02_ref/load_video_names_from_s3.py
@@ -1,6 +1,6 @@
 import os
 import json
-from src.traffic_analysis.d00_utils.data_retrieval import connect_to_bucket
+from traffic_analysis.d00_utils.data_retrieval import connect_to_bucket
 
 
 def load_video_names_from_s3(ref_file, paths):
