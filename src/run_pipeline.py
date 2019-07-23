70266834c985ed012837c1672573498009be2430
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-07-23 16:08:18 +0300

    use data loader

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -38,7 +38,8 @@ while selected_videos:
     update_frame_level_table(file_names=selected_videos[:chunk_size],
                              paths=paths,
                              params=params,
-                             creds=creds)
+                             creds=creds,
+                             s3_credentials=s3_credentials)
 
     # evaluate_frame_level_table
 
