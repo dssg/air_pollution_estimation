161ec42c082ad0a2e7b9f8338850f357835cd90a
SamBlakeman <sam.blakeman03@gmail.com>
2019-07-29 10:11:26 +0000

    Bug Fixes

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,6 +1,7 @@
 from traffic_analysis.d00_utils.data_retrieval import delete_and_recreate_dir
 from traffic_analysis.d00_utils.load_confs import load_parameters, load_paths, load_credentials
 from traffic_analysis.d02_ref.load_video_names_from_s3 import load_video_names_from_s3
+from traffic_analysis.d02_ref.retrieve_and_upload_video_names_to_s3 import retrieve_and_upload_video_names_to_s3
 from traffic_analysis.d03_processing.update_frame_level_table import update_frame_level_table
 from traffic_analysis.d03_processing.update_video_level_table import update_video_level_table
 from traffic_analysis.d04_modelling.trackinganalyser.trackinganalyser import TrackingAnalyser
@@ -14,21 +15,18 @@ analyzer = TrackingAnalyser(params=params, paths=paths)
 
 # If running first time:
 # creates the test_seach_json. Change the camera list and output file name for full run
-"""
-retrieve_and_upload_video_names_to_s3(ouput_file_name='test_search',
-                                      paths=paths,
-                                      s3_credentials=s3_credentials,
-                                      from_date='2019-06-30',
-                                      to_date='2019-06-30',
-                                      from_time='13-00-00',
-                                      to_time='13-05-00',
-                                      camera_list=params['tims_camera_list'][:2])
 
+retrieve_and_upload_video_names_to_s3(ouput_file_name='tims_cameras_all',
+                                      paths=paths,
+                                      from_date='2019-07-01',
+                                      s3_credentials=s3_credentials,
+                                      camera_list=params['tims_camera_list'][:5])
+"""
 upload_annotation_names_to_s3(paths=paths,
                               s3_credentials=s3_credentials)
 """
 # Start from here if video names already specified
-selected_videos = load_video_names_from_s3(ref_file='test_search',
+selected_videos = load_video_names_from_s3(ref_file='tims_cameras_all',
                                            paths=paths,
                                            s3_credentials=s3_credentials)
 
