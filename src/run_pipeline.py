40968802ae51d98933708900cc9304f069c8e7db
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-07-31 00:17:43 +0000

    proper formatting

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,45 +1,58 @@
 from traffic_analysis.d00_utils.data_retrieval import delete_and_recreate_dir
-from traffic_analysis.d00_utils.load_confs import load_parameters, load_paths, load_credentials
+from traffic_analysis.d00_utils.load_confs import (
+    load_parameters,
+    load_paths,
+    load_credentials,
+)
 from traffic_analysis.d02_ref.load_video_names_from_s3 import load_video_names_from_s3
-from traffic_analysis.d02_ref.retrieve_and_upload_video_names_to_s3 import retrieve_and_upload_video_names_to_s3
-from traffic_analysis.d02_ref.upload_annotation_names_to_s3 import upload_annotation_names_to_s3
-from traffic_analysis.d03_processing.update_frame_level_table import update_frame_level_table
+from traffic_analysis.d02_ref.retrieve_and_upload_video_names_to_s3 import (
+    retrieve_and_upload_video_names_to_s3,
+)
+from traffic_analysis.d02_ref.upload_annotation_names_to_s3 import (
+    upload_annotation_names_to_s3,
+)
+from traffic_analysis.d03_processing.update_frame_level_table import (
+    update_frame_level_table,
+)
 
 
 params = load_parameters()
 paths = load_paths()
 creds = load_credentials()
-s3_credentials = creds[paths['s3_creds']]
+s3_credentials = creds[paths["s3_creds"]]
 
 # If running first time:
 # creates the test_seach_json. Change the camera list and output file name for full run
-retrieve_and_upload_video_names_to_s3(ouput_file_name='test_search',
-                                      paths=paths,
-                                      s3_credentials=s3_credentials,
-                                      from_date='2019-06-30',
-                                      to_date='2019-06-30',
-                                      from_time='13-00-00',
-                                      to_time='13-05-00',
-                                      camera_list=params['tims_camera_list'][:2])
+retrieve_and_upload_video_names_to_s3(
+    ouput_file_name="test_search",
+    paths=paths,
+    s3_credentials=s3_credentials,
+    from_date="2019-06-30",
+    to_date="2019-06-30",
+    from_time="13-00-00",
+    to_time="13-05-00",
+    camera_list=params["tims_camera_list"][:2],
+)
 
-upload_annotation_names_to_s3(paths=paths,
-                              s3_credentials=s3_credentials)
+upload_annotation_names_to_s3(paths=paths, s3_credentials=s3_credentials)
 
 
 # Start from here if video names already specified
-selected_videos = load_video_names_from_s3(ref_file='test_search',
-                                           paths=paths,
-                                           s3_credentials=s3_credentials)
+selected_videos = load_video_names_from_s3(
+    ref_file="test_search", paths=paths, s3_credentials=s3_credentials
+)
 
 # select chunks of videos and classify objects
-chunk_size = params['chunk_size']
+chunk_size = params["chunk_size"]
 while selected_videos:
 
-    update_frame_level_table(file_names=selected_videos[:chunk_size],
-                             paths=paths,
-                             params=params,
-                             creds=creds,
-                             s3_credentials=s3_credentials)
+    update_frame_level_table(
+        file_names=selected_videos[:chunk_size],
+        paths=paths,
+        params=params,
+        creds=creds,
+        s3_credentials=s3_credentials,
+    )
 
     # evaluate_frame_level_table
 
@@ -61,12 +74,3 @@ annotations_df = parse_annotations(paths['annotations'], bool_print_summary=Fals
 count_differences_df = report_count_differences(annotations_df, yolo_df)
 count_differences_df.to_csv(paths['processed_video'] + 'JamCamCountDifferences.csv')
 """
-
-
-
-
-
-
-
-
-
