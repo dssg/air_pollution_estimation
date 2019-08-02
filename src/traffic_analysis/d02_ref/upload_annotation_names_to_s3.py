4715a2ad027756ec00ace54dc718f878added277
Kesa Oluwafunmilola <kesaoluwafunmilola@gmail.com>
2019-08-02 21:56:45 +0100

    reverted

--- src/traffic_analysis/d02_ref/upload_annotation_names_to_s3.py
+++ src/traffic_analysis/d02_ref/upload_annotation_names_to_s3.py
@@ -13,9 +13,9 @@ def upload_annotation_names_to_s3(paths,
 
         """
 
-    bucket_name = paths["bucket_name"]
-    s3_profile = paths["s3_profile"]
-    prefix = "%s" % (paths["s3_annotations"])
+    bucket_name = paths['bucket_name']
+    s3_profile = paths['s3_profile']
+    prefix = "%s" % (paths['s3_annotations'])
 
     # fetch video filenames
     elapsed_time, files = get_names_of_folder_content_from_s3(bucket_name, prefix, s3_profile)
@@ -25,16 +25,16 @@ def upload_annotation_names_to_s3(paths,
     selected_files = []
     for file in files:
         if file:
-            vals = file.split("_")
+            vals = file.split('_')
             if len(vals) == 4:
                 vals = vals[1:]
             date = vals[0]
-            time = vals[1].replace("-", ":")
-            name = date + " " + time + "_" + vals[2]
-            name = name.replace(".xml", ".mp4")
-            selected_files.append(paths["s3_video"] + date + "/" + name)
+            time = vals[1].replace('-', ':')
+            name = date + ' ' + time + '_' + vals[2]
+            name = name.replace('.xml', '.mp4')
+            selected_files.append(paths['s3_video'] + date + '/' + name)
 
     dl = DataLoaderS3(s3_credentials,
                       bucket_name=paths['bucket_name'])
-    file_path = paths["s3_video_names"] + "annotations.json"
+    file_path = paths['s3_video_names'] + 'annotations.json'
     dl.save_json(data=selected_files, file_path=file_path)
