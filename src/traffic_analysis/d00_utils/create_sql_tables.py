c07509aada03c889c2afd833ed91d45ee0674de7
MarenEckhoff <maren.eckhoff@quantumblack.com>
2019-08-02 11:29:45 +0100

    use paths to define which tables to create and use the data loader to execute the queries

--- src/traffic_analysis/d00_utils/create_sql_tables.py
+++ src/traffic_analysis/d00_utils/create_sql_tables.py
@@ -1,29 +1,31 @@
-import psycopg2
-
 from traffic_analysis.d00_utils.load_confs import load_credentials, load_paths
 from traffic_analysis.d00_utils.data_loader_sql import DataLoaderSQL
 
 
 def create_sql_tables(drop=False):
 
-    dl = DataLoaderSQL(creds=load_credentials(), paths=load_paths())
+    paths = load_paths()
 
+    # create queries
     drop_commands = None
     if drop:
         drop_commands = [
-            "DROP TABLE vehicle_types2, cameras2, frame_stats2, video_stats2 CASCADE;"
+            "DROP TABLE {}, {}, {}, {} CASCADE;".format(paths['db_vehicle_types'],
+                                                        paths['db_cameras'],
+                                                        paths['db_frame_level'],
+                                                        paths['db_video_level'])
         ]
 
     commands = [
         """
-        CREATE TABLE vehicle_types2(
+        CREATE TABLE {}(
             id SERIAL,
             vehicle_type VARCHAR(100),
             vehicle_type_id INTEGER PRIMARY KEY
         )
-        """,
+        """.format(paths['db_vehicle_types']),
         """
-        CREATE TABLE cameras2(
+        CREATE TABLE {}(
             id SERIAL,
             latitude FLOAT, 
             longitude FLOAT,
@@ -31,9 +33,9 @@ def create_sql_tables(drop=False):
             tfl_camera_id INTEGER,
             camera_name VARCHAR(100)
         )
-        """,
+        """.format(paths['db_cameras']),
         """
-        CREATE TABLE frame_stats2(
+        CREATE TABLE {}(
             camera_id VARCHAR(20),
             frame_id INTEGER,
             video_upload_datetime timestamp,
@@ -45,37 +47,25 @@ def create_sql_tables(drop=False):
             box_h INTEGER,
             creation_datetime timestamp
         )
-        """,
+        """.format(paths['db_frame_level']),
 
         """
-        CREATE TABLE video_stats2(
+        CREATE TABLE {}(
             vehicle_type VARCHAR(100),
             camera_id VARCHAR(20),
             video_upload_datetime timestamp,
             counts FLOAT,
             creation_datetime timestamp
         )
-        """
+        """.format(paths['db_video_level'])
     ]
 
-    try:
-        dl.open_connection()
+    # execute queries
+    dl = DataLoaderSQL(creds=load_credentials(), paths=paths)
 
-        if(drop_commands is not None):
-            for command in drop_commands:
-                dl.cursor.execute(command)
+    if drop_commands is not None:
+        for command in drop_commands:
+            dl.execute_raw_sql_query(sql=command)
 
-        for command in commands:
-            dl.cursor.execute(command)
-
-        dl.conn.commit()
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        return False
-
-    finally:
-        dl.cursor.close()
-        if dl.conn:
-            dl.conn.close()
-
-    return True
+    for command in commands:
+        dl.execute_raw_sql_query(sql=command)
