7ea9cfa0cd0b4a44b2be6b584ecf452d0ce1b9de
Sam Blakeman <sam.blakeman03@gmail.com>
2019-08-02 15:32:09 +0100

    More parsing

--- src/traffic_analysis/d04_modelling/transfer/data_loader.py
+++ src/traffic_analysis/d04_modelling/transfer/data_loader.py
@@ -1,5 +1,8 @@
+import xml.etree.ElementTree as ET
 
+from traffic_analysis.d00_utils.load_confs import load_paths, load_credentials
 from traffic_analysis.d00_utils.data_loader_s3 import DataLoaderS3
+
 
 from enum import Enum
 class TransferDataset(Enum):
@@ -15,7 +18,9 @@ class DataLoader(object):
         self.paths = paths
         self.parse_mapping = {TransferDataset.detrac: self.parse_detrac_data,
                               TransferDataset.cvat: self.parse_cvat_data}
-        self.data_loader_s3 = DataLoaderS3(s3_credentials=, bucket_name=)
+
+        self.data_loader_s3 = DataLoaderS3(s3_credentials=creds[paths['s3_creds']],
+                                           bucket_name=paths['bucket_name'])
 
         return
 
@@ -32,6 +37,28 @@ class DataLoader(object):
 
         print('Parsing detrac dataset...')
 
+        xml_files = self.data_loader_s3.list_objects(prefix=self.paths['s3_detrac_annotations'])
+        for xml_file in xml_files:
+            self.parse_detrac_xml_file(xml_file)
+
+        return
+
+    def parse_detrac_xml_file(self, xml_file):
+
+        path = self.paths['temp_annotation'] + xml_file.split('/')[-1]
+
+        self.data_loader_s3.download_file(path_of_file_to_download=xml_file,
+                                          path_to_download_file_to=path)
+
+        tree = ET.parse(xml_file.split('/')[-1])
+        img_name = path.split('/')[-1][:-4]
+
+        height = tree.findtext("./size/height")
+        width = tree.findtext("./size/width")
+
+        objects = [img_name, width, height]
+        print(objects)
+
         return
 
     def parse_cvat_data(self):
@@ -41,5 +68,8 @@ class DataLoader(object):
         return
 
 
+paths = load_paths()
+creds = load_credentials()
 
-dl = DataLoader(datasets=[], creds=creds, paths=paths)
+dl = DataLoader(datasets=[TransferDataset.detrac], creds=creds, paths=paths)
+dl.get_train_and_test(.8)
