60594812c3c1639678ff0e903f9a41e3d42b4aef
Kesa Oluwafunmilola <kesaoluwafunmilola@gmail.com>
2019-08-02 21:00:52 +0100

    reversed formatting

--- src/traffic_analysis/d02_ref/load_video_names_from_s3.py
+++ src/traffic_analysis/d02_ref/load_video_names_from_s3.py
@@ -1,7 +1,9 @@
 from traffic_analysis.d00_utils.data_loader_s3 import DataLoaderS3
 
 
-def load_video_names_from_s3(ref_file, paths, s3_credentials: dict):
+def load_video_names_from_s3(ref_file,
+                             paths,
+                             s3_credentials: dict):
     """ Load the json file from ref on s3
             Args:
                 ref_file (str): name of the reference file to be loaded
@@ -12,15 +14,14 @@ def load_video_names_from_s3(ref_file, paths, s3_credentials: dict):
                 files (list): list of files to be downloaded from s3
         """
 
-    dl = DataLoaderS3(s3_credentials=s3_credentials, bucket_name=paths["bucket_name"])
-
+    dl = DataLoaderS3(s3_credentials=s3_credentials,
+                      bucket_name=paths['bucket_name'])
+    
     files = dl.read_json(file_path=paths["s3_video_names"] + ref_file + ".json")
 
     annotations_path = paths["s3_video_names"] + "annotations.json"
     if dl.file_exists(annotations_path):
-        annotation_video_names = dl.read_json(
-            file_path=paths["s3_video_names"] + "annotations.json"
-        )
+        annotation_video_names = dl.read_json(file_path=paths['s3_video_names'] + 'annotations.json')
         files += annotation_video_names
 
     # avoid duplication
