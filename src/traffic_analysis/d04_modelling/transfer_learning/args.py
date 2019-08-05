457ad36f466650ebc315d64ac3a8286d5d039830
jackattack1415 <jhensley@g.harvard.edu>
2019-08-05 14:22:42 +0100

    basic arg parser function added

--- src/traffic_analysis/d04_modelling/transfer_learning/args.py
+++ src/traffic_analysis/d04_modelling/transfer_learning/args.py
@@ -78,7 +78,9 @@ warm_up_epoch = 3  # Warm up training epoches. Set to a larger value if gradient
 ### some constants in validation
 # nms
 nms_threshold = 0.45  # iou threshold in nms operation
-score_threshold = 0.01  # threshold of the probability of the classes in nms operation, i.e. score = pred_confs * pred_probs. set lower for higher recall.
+# threshold of the probability of the classes in nms operation,
+# i.e. score = pred_confs * pred_probs. set lower for higher recall.
+score_threshold = 0.01
 nms_topk = 150  # keep at most nms_topk outputs after nms
 # mAP eval
 eval_threshold = 0.5  # the iou threshold applied in mAP evaluation
