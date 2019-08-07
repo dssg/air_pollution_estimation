688e3b01363346789a7373fd04456c41bc494b40
Sam Blakeman <sam.blakeman03@gmail.com>
2019-08-07 11:51:23 +0100

    More parsing of both the input and output data for transfer learning

--- src/traffic_analysis/d04_modelling/transfer/data_loader.py
+++ src/traffic_analysis/d04_modelling/transfer/data_loader.py
@@ -1,4 +1,7 @@
 import xml.etree.ElementTree as ET
+from random import shuffle
+from PIL import Image
+import numpy as np
 
 from traffic_analysis.d00_utils.load_confs import load_paths, load_credentials
 from traffic_analysis.d00_utils.data_loader_s3 import DataLoaderS3
@@ -17,8 +20,8 @@ class DataLoader(object):
         self.datasets = datasets
         self.creds = creds
         self.paths = paths
-        self.parse_mapping = {TransferDataset.detrac: self.parse_detrac_data,
-                              TransferDataset.cvat: self.parse_cvat_data}
+        self.load_mapping = {TransferDataset.detrac: self.load_detrac_data,
+                             TransferDataset.cvat: self.load_cvat_data}
 
         self.data_loader_s3 = DataLoaderS3(s3_credentials=creds[paths['s3_creds']],
                                            bucket_name=paths['bucket_name'])
@@ -28,27 +31,71 @@ class DataLoader(object):
 
     def get_train_and_test(self, train_fraction):
 
-        results = []
         delete_and_recreate_dir(self.paths['temp_annotation'])
+        delete_and_recreate_dir(self.paths['temp_raw_images'])
+        x, y = self.load_data_from_s3()
+        delete_and_recreate_dir(self.paths['temp_annotation'])
+        delete_and_recreate_dir(self.paths['temp_raw_images'])
+
+        """
+        results = shuffle(results)
+        train = results[:int(train_fraction * len(results))]
+        test = results[int(train_fraction * len(results)):]
+        """
+
+        return #train_x, train_y, test_x, test_y
+
+    def load_data_from_s3(self):
+
+        xs = []
+        ys = []
 
         for dataset in self.datasets:
-            results += self.parse_mapping[dataset]()
+            x, y = self.load_mapping[dataset]()
+            xs += x
+            ys += y
 
-            delete_and_recreate_dir(self.paths['temp_annotation'])
+        return x, y
 
-        return
+    def load_inputs_from_s3(self, test_y, train_y):
 
-    def parse_detrac_data(self):
+        test_x = []
+        train_x = []
 
-        print('Parsing detrac dataset...')
-        results = []
+        return test_x, train_x
+
+    def load_detrac_data(self):
+
+        print('Parsing detrac xmls...')
+        y = []
         xml_files = self.data_loader_s3.list_objects(prefix=self.paths['s3_detrac_annotations'])
         for xml_file in xml_files:
             result = self.parse_detrac_xml_file(xml_file)
             if (result):
-                results += result
+                y += result
 
-        return results
+        print('Loading detrac images...')
+        x = []
+        for labels in y:
+            print(labels)
+            image_num = labels.split(' ')[0].zfill(5)
+            folder = labels.split(' ')[1]
+            file_to_download = paths['s3_detrac_images'] + \
+                               folder + '/' + \
+                               'img' + image_num + '.jpg'
+            download_file_to = paths['temp_raw_images'] + \
+                               folder + '_' + \
+                               image_num + '.jpg'
+
+            self.data_loader_s3.download_file(
+                path_of_file_to_download=file_to_download,
+                path_to_download_file_to=download_file_to)
+
+            img = Image.open(download_file_to)
+            img.load()
+            x.append(np.asarray(img, dtype="int32"))
+
+        return x, y
 
     def parse_detrac_xml_file(self, xml_file):
 
@@ -110,17 +157,38 @@ class DataLoader(object):
         else:
             return None
 
-    def parse_cvat_data(self):
+    def load_cvat_data(self):
 
-        print('Parsing cvat dataset...')
-        results = []
+        print('Parsing cvat xmls...')
+        y = []
         xml_files = self.data_loader_s3.list_objects(prefix=self.paths['s3_cvat_annotations'])
         for xml_file in xml_files:
             result = self.parse_cvat_xml_file(xml_file)
             if(result):
-                results += result
+                y += result
 
-        return results
+        print('Loading cvat videos...')
+        x = []
+        for labels in y:
+            print(labels)
+            image_num = labels.split(' ')[0].zfill(5)
+            folder = labels.split(' ')[1]
+            file_to_download = paths['s3_detrac_images'] + \
+                               folder + '/' + \
+                               'img' + image_num + '.jpg'
+            download_file_to = paths['temp_raw_images'] + \
+                               folder + '_' + \
+                               image_num + '.jpg'
+
+            self.data_loader_s3.download_file(
+                path_of_file_to_download=file_to_download,
+                path_to_download_file_to=download_file_to)
+
+            img = Image.open(download_file_to)
+            img.load()
+            x.append(np.asarray(img, dtype="int32"))
+
+        return x, y
 
     def parse_cvat_xml_file(self, xml_file):
 
