6eb4179863a3edb4ca40198636be302f3cacb593
Caroline <carolinewang01@gmail.com>
2019-08-12 12:03:11 +0100

    further formatting changes and debug statements

--- src/eval_pipeline.py
+++ src/eval_pipeline.py
@@ -8,7 +8,7 @@ from traffic_analysis.d02_ref.upload_annotation_names_to_s3 import upload_annota
 from traffic_analysis.d03_processing.update_frame_level_table import update_frame_level_table
 from traffic_analysis.d03_processing.update_video_level_table import update_video_level_table
 from traffic_analysis.d03_processing.update_eval_tables import update_eval_tables
-from traffic.analysis.d03_processing.create_traffic_analysers import create_traffic_analysers
+from traffic_analysis.d03_processing.create_traffic_analysers import create_traffic_analysers
 
 params = load_parameters()
 paths = load_paths()
@@ -20,7 +20,8 @@ verbose = True
 
 # pipeline start 
 traffic_analysers = create_traffic_analysers(params=params,
-                                             paths=paths)
+                                             paths=paths,
+                                             verbose=verbose)
 # If running first time:
 # get annotation xmls from s3 saves json on s3 containing to corresponding video filepaths
 annotations_videos_name_mapper = upload_annotation_names_to_s3(paths=paths,
@@ -31,15 +32,17 @@ annotations_videos_name_mapper = upload_annotation_names_to_s3(paths=paths,
 selected_videos = load_video_names_from_s3(ref_file= params['eval_ref_name'],
                                            paths=paths,
                                            s3_credentials=s3_credentials)
+
 if verbose: print("Successfully loaded selected videos")
+
 # create eval tables if they don't exist
 create_eval_sql_tables(creds=creds,
                        paths=paths, 
                        drop=False)
 
 if verbose: print("Running evaluation for traffic analysers: ", traffic_analysers.keys())
-selected_videos_master = selected_videos.copy()
 
+selected_videos_master = selected_videos.copy()
 for analyser_name, traffic_analyser in traffic_analysers.items():
     db_frame_level_name = f"{paths['eval_db_frame_level_prefix']}_{analyser_name}"
     db_video_level_name = f"{paths['eval_db_video_level_prefix']}_{analyser_name}"
@@ -63,6 +66,7 @@ for analyser_name, traffic_analyser in traffic_analysers.items():
                                                            paths=paths,
                                                            creds=creds)
         analyser_runtime += runtime_list
+
         if success:
             video_level_df = update_video_level_table(analyser=traffic_analyser,
                                    db_video_level_name=db_video_level_name,
@@ -83,8 +87,10 @@ for analyser_name, traffic_analyser in traffic_analysers.items():
         selected_videos = selected_videos[chunk_size:]
         delete_and_recreate_dir(paths["temp_video"])
 
-    if verbose: print(f"Successfully processed videos for traffic analyser: {analyser_name}")
-    print(f"Avg runtime of one video for tracking type {analyser_name}: {np.mean(np.array(analyser_runtime))}")
+    if verbose: 
+      print(f"Successfully processed videos for traffic analyser: {analyser_name}")
+      print(f"Avg runtime of one video for tracking type {analyser_name}: {np.mean(np.array(analyser_runtime))}")
+      
     # append to table 
     update_eval_tables(db_frame_level_name=db_frame_level_name, 
                        db_video_level_name=db_video_level_name,
@@ -94,4 +100,5 @@ for analyser_name, traffic_analyser in traffic_analysers.items():
                        paths=paths,
                        analyser_type=analyser_name
                        )
+
     if verbose: print("Successfully evaluated videos for one tracking type")
