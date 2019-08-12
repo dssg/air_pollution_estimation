162dbb5d4eb69c7cf86d799d898d7e2b35425d52
Caroline <carolinewang01@gmail.com>
2019-08-12 11:45:35 +0100

    put the creation of traffic analysers into sep file

--- src/eval_pipeline.py
+++ src/eval_pipeline.py
@@ -1,3 +1,5 @@
+import numpy as np 
+
 from traffic_analysis.d00_utils.data_retrieval import delete_and_recreate_dir
 from traffic_analysis.d00_utils.load_confs import load_parameters, load_paths, load_credentials
 from traffic_analysis.d00_utils.create_sql_tables import create_primary_sql_tables, create_eval_sql_tables
@@ -6,44 +8,38 @@ from traffic_analysis.d02_ref.upload_annotation_names_to_s3 import upload_annota
 from traffic_analysis.d03_processing.update_frame_level_table import update_frame_level_table
 from traffic_analysis.d03_processing.update_video_level_table import update_video_level_table
 from traffic_analysis.d03_processing.update_eval_tables import update_eval_tables
-from traffic_analysis.d04_modelling.tracking.tracking_analyser import TrackingAnalyser
-import numpy as np 
+from traffic.analysis.d03_processing.create_traffic_analysers import create_traffic_analysers
 
 params = load_parameters()
 paths = load_paths()
 creds = load_credentials()
 s3_credentials = creds[paths['s3_creds']]
-verbose = True
-# initialize traffic analysers with various tracker types 
-traffic_analysers = {}
-for tracker_type in params["eval_tracker_types"]:
-    analyser_name = params["traffic_analyser"].lower()
-    traffic_analysers[ f"{analyser_name}_{tracker_type}"] = \
-        eval(params["traffic_analyser"])(params=params, 
-                                         paths=paths, 
-                                         tracker_type=tracker_type,
-                                         verbose=verbose)
 
+# settings
+verbose = True
+
+# pipeline start 
+traffic_analysers = create_traffic_analysers(params=params,
+                                             paths=paths)
 # If running first time:
-# creates the test_seach_json. Change the camera list and output file name for full run
 # get annotation xmls from s3 saves json on s3 containing to corresponding video filepaths
 annotations_videos_name_mapper = upload_annotation_names_to_s3(paths=paths,
          		                        output_file_name=params['eval_ref_name'],
                                     s3_credentials=s3_credentials,
                                     verbose=verbose)
 
-# Start from here if video names already specified
 selected_videos = load_video_names_from_s3(ref_file= params['eval_ref_name'],
                                            paths=paths,
                                            s3_credentials=s3_credentials)
 if verbose: print("Successfully loaded selected videos")
-# wipe and recreate eval tables 
+# create eval tables if they don't exist
 create_eval_sql_tables(creds=creds,
                        paths=paths, 
                        drop=False)
 
-selected_videos_master = selected_videos.copy()
 if verbose: print("Running evaluation for traffic analysers: ", traffic_analysers.keys())
+selected_videos_master = selected_videos.copy()
+
 for analyser_name, traffic_analyser in traffic_analysers.items():
     db_frame_level_name = f"{paths['eval_db_frame_level_prefix']}_{analyser_name}"
     db_video_level_name = f"{paths['eval_db_video_level_prefix']}_{analyser_name}"
