1ea2edbbf9210f6ed44f91a2322d3d94dcd08b0c
Aruza11 <carolinewang01@gmail.com>
2019-08-12 15:52:02 +0000

    minor bug fixes

--- src/traffic_analysis/d03_processing/create_traffic_analysers.py
+++ src/traffic_analysis/d03_processing/create_traffic_analysers.py
@@ -13,8 +13,9 @@ def create_traffic_analysers(params: dict,
     for tracker_type in params["eval_tracker_types"]:
         analyser_name = params["traffic_analyser"].lower()
         traffic_analysers[ f"{analyser_name}_{tracker_type}"] = \
-            eval(params["traffic_analyser"])(params=params, 
-                                             paths=paths, 
+            eval(params["traffic_analyser"])(params=params,
+                                             paths=paths,
+                                             s3_credentials=s3_credentials,
                                              tracker_type=tracker_type,
                                              verbose=verbose)
 
