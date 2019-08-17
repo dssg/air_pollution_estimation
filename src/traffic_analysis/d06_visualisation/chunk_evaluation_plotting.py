93a89cd9e33471670079402d03dfd37f7c7ae702
Caroline <carolinewang01@gmail.com>
2019-08-17 11:41:29 +0100

    in process of incorporating maren changes

--- src/traffic_analysis/d06_visualisation/chunk_evaluation_plotting.py
+++ src/traffic_analysis/d06_visualisation/chunk_evaluation_plotting.py
@@ -1,41 +1,49 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from math import ceil
 
-
-def plot_video_stats_diff_distribution(video_level_diff_df: pd.DataFrame,
-                                       video_stat_types: list, 
+def plot_video_stats_diff_distribution(video_level_diff: pd.DataFrame,
+                                       video_stat_types: list = ["counts", "stops", "starts"], 
                                        show_plot: bool = True,
                                        save_path: str = None):
     """For each statistic type (counts, starts, stops, etc.) plots the
     distribution of difference from ground truth for each vehicle type
 
     Args:
-        video_level_diff_df: the output of ChunkEvaluator.evaluate_video_level()
+        video_level_diff: the output of ChunkEvaluator.evaluate_video_level()
         video_stat_types: list of video level stats computed (pass from params)
         show_plot: If true, will display the plot
         save_path: if specified, will save to this location (specify full path with
         desired filename)
     """
     plt.style.use('seaborn-deep')
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(30,25))
-    
-    for i,stat_type in enumerate(video_stat_types):
-        stat_list = []
-        vehicle_types=[]
-        for vehicle_type, vehicle_group_df in video_level_diff_df.groupby("vehicle_type"):
-            stat_list.append(vehicle_group_df[f"{stat_type}_diff"])
-            vehicle_types.append(vehicle_type)
+    num_stats = len(video_stat_types)
+    fig, axes = plt.subplots(nrows=ceil(num_stats/2), 
+                             ncols=2, 
+                             figsize=(30,25))
+    i = 0 
+    for row in axes: 
+        for ax in row: 
+            if i >= num_stats:
+                continue
+            stat_type = video_stat_types[i]
+            stat_list = []
+            vehicle_types=[]
+            for vehicle_type, vehicle_group_df in video_level_diff.groupby("vehicle_type"):
+                stat_list.append(vehicle_group_df[f"{stat_type}_diff"])
+                vehicle_types.append(vehicle_type)
 
-        # plot distribution of stat_type on one sub-plot
-        ax = eval(f"ax{i+1}")
-        ax.set_title(stat_type, fontsize = 40)
-        ax.hist(x=stat_list, 
-                label=vehicle_types,
-                alpha=0.8)
-        ax.legend(loc="upper right")
+            # plot distribution of stat_type on one sub-plot
+            ax.set_title(stat_type, fontsize = 32)
+            ax.hist(x=stat_list, 
+                    label=vehicle_types,
+                    alpha=0.8)
+            ax.legend(loc="upper right")
+            
+            i += 1
 
-    fig.suptitle("Distribution of Differences for Traffic Statistics", size=32)
-    fig.text(0, 0.5,"Frequency", ha='center', va='center', rotation='vertical', size=32) #y label
+    fig.suptitle("Distribution of Differences for Traffic Statistics", size=40)
+    fig.text(0, 0.5,"Frequency", ha='center', va='center', rotation='vertical', size=32) # y label
     fig.text(0.5, 0, "Model Prediction", ha='center', va='center', size=32) # x label
 
     fig.tight_layout()
@@ -49,7 +57,7 @@ def plot_video_stats_diff_distribution(video_level_diff_df: pd.DataFrame,
     if show_plot: 
         plt.show()
     plt.close()
-    
+
 
 def plot_video_level_summary_stats(video_level_stats_df: pd.DataFrame, 
                                    metrics = {'bias':None, 'rmse':None, 'mae': None},
