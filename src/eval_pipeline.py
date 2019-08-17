93ea1d35d58a6c1738b528038237f3be840f61a0
Caroline <carolinewang01@gmail.com>
2019-08-17 12:24:59 +0100

    added more bbox formatting ocrrections, addedd looping thru param sets

--- src/eval_pipeline.py
+++ src/eval_pipeline.py
@@ -23,7 +23,7 @@ verbose = True
 # pipeline start
 
 # get param grids
-traffic_analysers_params = initialize_param_sets(params=params)
+traffic_analysers_params, num_params = initialize_param_sets(params=params)
 
 # If running first time:
 # get annotation xmls from s3 saves json on s3 containing to corresponding video filepaths
@@ -48,80 +48,93 @@ if verbose:
     print("Running evaluation for traffic analysers: ", traffic_analysers_params.keys())
 
 selected_videos_master = selected_videos.copy()
-for i, (analyser_name, params_to_set_dict) in enumerate(traffic_analysers_params.items()):
-    if verbose:
-        print(f"Now evaluating {analyser_name} with param set: {params_to_set_dict}")
+param_counter = 1
 
-    # initialize db names
-    db_frame_level_name = f"{paths['eval_db_frame_level_prefix']}_{analyser_name}_{i}"
-    db_video_level_name = f"{paths['eval_db_video_level_prefix']}_{analyser_name}_{i}"
+for analyser_name, params_subgrid in traffic_analysers_params.items():
+    # iterate thru tuneable params for each combo of detection model/tracking type
+    for params_to_set_dict in params_subgrid:
+        if verbose:
+            print(f"Now evaluating param set {param_counter}/{num_params}")
 
-    # wipe and recreate video/frame level stats tables for tracker types
-    create_primary_sql_tables(db_frame_level_name=db_frame_level_name,
-                              db_video_level_name=db_video_level_name,
-                              drop=True)
+        # initialize db names
+        db_frame_level_name = f"{paths['eval_db_frame_level_prefix']}_{analyser_name}"
+        db_video_level_name = f"{paths['eval_db_video_level_prefix']}_{analyser_name}"
 
-    # create traffic analyser
-    traffic_analyser = create_traffic_analyser(params_to_set=params_to_set_dict,
-                                               params=params,
-                                               paths=paths,
-                                               s3_credentials=s3_credentials,
-                                               verbose=verbose)
+        # wipe and recreate video/frame level stats tables for tracker types
+        create_primary_sql_tables(db_frame_level_name=db_frame_level_name,
+                                  db_video_level_name=db_video_level_name,
+                                  drop=True)
 
-    # select chunks of videos and classify objects
-    chunk_size = params['eval_chunk_size']
-    chunk_counter = 0
-    analyser_runtime = []
+        # create traffic analyser
+        traffic_analyser = create_traffic_analyser(params_to_set=params_to_set_dict,
+                                                   params=params,
+                                                   paths=paths,
+                                                   s3_credentials=s3_credentials,
+                                                   verbose=verbose)
 
-    # regenerate selected videos
-    selected_videos = selected_videos_master
-    while selected_videos:
+        # select chunks of videos and classify objects
+        chunk_size = params['eval_chunk_size']
+        chunk_counter = 0
+        analyser_runtime = []
+
+        # regenerate selected videos
+        selected_videos = selected_videos_master
+        while selected_videos:
+            try:
+                success, frame_level_df, runtime_list, lost_tracking = update_frame_level_table(analyser=traffic_analyser,
+                                                                                 file_names=selected_videos[:chunk_size],
+                                                                                 db_frame_level_name=db_frame_level_name,
+                                                                                 paths=paths,
+                                                                                 creds=creds)
+                analyser_runtime += runtime_list
+
+                if success:
+                    video_level_df = update_video_level_table(analyser=traffic_analyser,
+                                                              db_video_level_name=db_video_level_name,
+                                                              frame_level_df=frame_level_df,
+                                                              file_names=selected_videos[:chunk_size],
+                                                              lost_tracking=lost_tracking,
+                                                              paths=paths,
+                                                              creds=creds,
+                                                              return_data=True)
+
+                    if verbose:
+                        print(f"Successfully processed chunk {chunk_counter}")
+            except Exception as e:
+                print(e)
+                print("Analysing current chunk failed. Continuing to next chunk.")
+                pass
+
+            chunk_counter += 1
+            selected_videos = selected_videos[chunk_size:]
+            delete_and_recreate_dir(paths["temp_video"])
+
+        avg_runtime = np.mean(np.array(analyser_runtime))
+        
+        if verbose:
+            print(f"Successfully processed videos for traffic analyser {analyser_name}, param set {params_to_set_dict}")
+            print(f"Avg runtime of one video: {avg_runtime}")
+            print(f"Now evaluating param set {param_counter}/{num_params}")
+
+
+        traffic_analyser.cleanup_on_finish()
+        
+        # append to table
         try:
-            success, frame_level_df, runtime_list, lost_tracking = update_frame_level_table(analyser=traffic_analyser,
-                                                                             file_names=selected_videos[:chunk_size],
-                                                                             db_frame_level_name=db_frame_level_name,
-                                                                             paths=paths,
-                                                                             creds=creds)
-            analyser_runtime += runtime_list
-
-            if success:
-                video_level_df = update_video_level_table(analyser=traffic_analyser,
-                                                          db_video_level_name=db_video_level_name,
-                                                          frame_level_df=frame_level_df,
-                                                          file_names=selected_videos[:chunk_size],
-                                                          lost_tracking=lost_tracking,
-                                                          paths=paths,
-                                                          creds=creds,
-                                                          return_data=True)
-
-                if verbose:
-                    print(f"Successfully processed chunk {chunk_counter}")
-        except Exception as e:
+            update_eval_tables(db_frame_level_name=db_frame_level_name,
+                             db_video_level_name=db_video_level_name,
+                             params=params,
+                             creds=creds,
+                             paths=paths,
+                             avg_runtime=avg_runtime,
+                             evaluated_params=params_to_set_dict
+                             )
+        except Exception as e: 
             print(e)
-            print("Analysing current chunk failed. Continuing to next chunk.")
-            pass
+            print(f"Failed to evaluate videos for traffic_analyser {analyser_name}, param set {params_to_set_dict}")
+            continue 
 
-        chunk_counter += 1
-        selected_videos = selected_videos[chunk_size:]
-        delete_and_recreate_dir(paths["temp_video"])
+        if verbose:
+            print(f"Successfully evaluated videos for traffic_analyser {analyser_name}, param set {params_to_set_dict}")
 
-    avg_runtime = np.mean(np.array(analyser_runtime))
-    
-    if verbose:
-        print(f"Successfully processed videos for traffic analyser: {analyser_name}")
-        print(f"Avg runtime of one video for tracking type {analyser_name}: {avg_runtime}")
-
-    traffic_analyser.cleanup_on_finish()
-    
-    # append to table
-    update_eval_tables(db_frame_level_name=db_frame_level_name,
-                       db_video_level_name=db_video_level_name,
-                       params=params,
-                       creds=creds,
-                       paths=paths,
-                       avg_runtime=avg_runtime,
-                       evaluated_params=params_to_set_dict
-                       )
-
-    if verbose:
-        print("Successfully evaluated videos for one tracking type")
+        param_counter += 1
