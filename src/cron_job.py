cf387373dde94cd0baa51a10cc8a6aef20031ca5
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-08-19 09:45:49 +0000

    changed how we retrieve paths

--- src/cron_job.py
+++ src/cron_job.py
@@ -1,40 +1,37 @@
 from crontab import CronTab
-from pathlib import Path
 import os
 from traffic_analysis.d00_utils.load_confs import load_paths
+from traffic_analysis.d00_utils.get_project_directory import get_project_directory
 
+project_directory = os.path.join(*get_project_directory())
 paths = load_paths()
-home = Path.home()
 python_path = "python3"
 
 
 class CronJobs:
     def __init__(self, username):
         sys_path = os.environ.get("PATH")
-        print(sys_path)
         self.cron = CronTab(user=username,
-                            log="/tmp/cron.log")
+                            log=os.path.join("tmp", "cron.log"))
         self.cron.env['PATH'] = sys_path
 
     def pipeline_job(self):
-        filename = home.joinpath(
-            "air_pollution_estimation/src/live_pipeline.py")
+        filename = os.path.join(project_directory, "src", "live_pipeline.py")
+        print(filename)
         job = self.cron.new(
             command=f"{python_path} {filename} >> /tmp/pipeline.log 2>&1", comment="pipeline")
         job.minute.every(4)
         self.cron.write_to_user()
 
     def download_videos_job(self):
-        filename = home.joinpath(
-            "air_pollution_estimation/src/download_videos.py")
+        filename = os.path.join(project_directory, "src", "download_videos.py")
         job = self.cron.new(
             command=f"{python_path} {filename} >> /tmp/download.log 2>&1", comment="download")
         job.minute.every(4)
         self.cron.write()
 
     def upload_videos_job(self):
-        filename = home.joinpath(
-            "air_pollution_estimation/src/upload_videos.py")
+        filename = os.path.join(project_directory, "src", "upload_videos.py")
         job = self.cron.new(
             command=f"{python_path} {filename} >> /tmp/upload.log 2>&1", comment="upload")
         job.minute.every(1)
@@ -54,14 +51,6 @@ class CronJobs:
 
     def remove_jobs_by_comment(self, job_comment):
         self.cron.remove_all(comment=job_comment)
-
-    def test(self):
-        filename = home.joinpath("air_pollution_estimation/src/test.py")
-        filename = "env >> /tmp/cron.log"
-        job = self.cron.new(
-            command=f"{filename}", comment="test")
-        job.minute.every(1)
-        self.cron.write()
 
 
 if __name__ == "__main__":
