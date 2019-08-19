ae023064c7e4006eae389f6e9d967633a463e7f9
SamBlakeman <sam.blakeman03@gmail.com>
2019-08-19 14:51:42 +0000

    finished uploading of annotations to S3

--- src/traffic_analysis/d00_utils/upload_setup_data_to_s3.py
+++ src/traffic_analysis/d00_utils/upload_setup_data_to_s3.py
@@ -67,16 +67,15 @@ def upload_yolo_weights_to_s3(s3_credentials,
     shutil.rmtree(local_dir)
 
 def upload_annotations_to_s3(s3_credentials, paths):
-    
     data_loader = DataLoaderS3(s3_credentials=s3_credentials, bucket_name=paths["bucket_name"])
 
     # raw videos
     for video_file in glob.glob(paths["setup_video"] + "*.mp4"):
         camera_id, video_upload_datetime = parse_video_or_annotation_name(video_name=video_file.split('/')[-1])
-        data_loader.upload_file(path_of_file_to_upload=video_file, path_to_upload_file_to=paths["s3_video"] + "date_test/" + str(video_upload_datetime) + "/" + video_file.split('/')[-1])
+        data_loader.upload_file(path_of_file_to_upload=video_file, path_to_upload_file_to=paths["s3_video"] + video_upload_datetime.strftime("%Y-%m-%d") + "/" + video_file.split('/')[-1])
 
     # xml files
     for xml_file in glob.glob(paths["setup_xml"] + "*.xml"):
-        data_loader.upload_file(path_of_file_to_upload=xml_file, path_to_upload_file_to=paths["s3_annotations"] + "cvat_test/" + xml_file.split('/')[-1])
+        data_loader.upload_file(path_of_file_to_upload=xml_file, path_to_upload_file_to=paths["s3_annotations"] + "cvat/" + xml_file.split('/')[-1])
 
     return
