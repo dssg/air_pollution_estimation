e1eb0dd7c5b6f8877f8eb015d40ad528825a2611
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-08-20 09:45:54 +0000

    moved download camera details to setup

--- src/download_videos.py
+++ src/download_videos.py
@@ -1,5 +1,5 @@
 from traffic_analysis.d00_utils.load_confs import load_parameters, load_credentials, load_paths
-from traffic_analysis.d01_data.collect_video_data import download_camera_meta_data, collect_camera_videos
+from traffic_analysis.d01_data.collect_video_data import collect_camera_videos
 
 params = load_parameters()
 creds = load_credentials()
@@ -7,12 +7,6 @@ paths = load_paths()
 
 tfl_camera_api = params['tfl_camera_api']
 s3_credentials = creds[paths['s3_creds']]
-
-# download camera data from tfl
-download_camera_meta_data(tfl_camera_api=tfl_camera_api,
-                          s3_credentials=s3_credentials)
-print("Downloaded tfl camera details.")
-
 
 collect_camera_videos(download_url=params['jamcam_url'],
                       s3_credentials=s3_credentials,
--- src/setup.py
+++ src/setup.py
@@ -2,19 +2,24 @@ from traffic_analysis.d00_utils.load_confs import load_paths, load_parameters, l
 from traffic_analysis.d00_utils.create_sql_tables import create_sql_tables
 from traffic_analysis.d00_utils.upload_setup_data_to_s3 import upload_yolo_weights_to_s3
 from traffic_analysis.d00_utils.upload_setup_data_to_s3 import upload_annotations_to_s3
+from traffic_analysis.d01_data.collect_video_data import download_camera_meta_data
 
 params = load_parameters()
 paths = load_paths()
 creds = load_credentials()
 s3_credentials = creds[paths['s3_creds']]
 
-# get yolo model weights and insert to SQL 
+# download camera data from tfl
+download_camera_meta_data(tfl_camera_api=tfl_camera_api,
+                          s3_credentials=s3_credentials)
+
+# get yolo model weights and insert to SQL
 upload_yolo_weights_to_s3(s3_credentials=s3_credentials,
                           bucket_name=paths['bucket_name'],
                           local_dir=paths['temp_setup'],
                           target_dir_on_s3=paths['s3_detection_model'])
 
-# create PSQL tables to insert vehicle statistics into 
+# create PSQL tables to insert vehicle statistics into
 create_sql_tables(drop=False)
 
 # put annotated videos in S3, put annotation xmls in right folder
