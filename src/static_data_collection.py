41981f00fce2e28146b0b20751a5a703c091f550
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-08-21 15:46:46 +0000

    renamed file and added upload function.

---
+++ src/static_data_collection.py
@@ -0,0 +1,38 @@
+from traffic_analysis.d00_utils.load_confs import load_parameters, load_credentials, load_paths
+from traffic_analysis.d01_data.collect_video_data import collect_camera_videos, upload_videos
+from multiprocessing import Process
+
+params = load_parameters()
+creds = load_credentials()
+paths = load_paths()
+
+s3_credentials = creds[paths['s3_creds']]
+iterations = params['iterations']
+delay = params['delay']
+
+
+def collect_camera_videos_fn():
+    collect_camera_videos(download_url=params['jamcam_url'],
+                          s3_credentials=s3_credentials,
+                          iterations=params["iterations"],
+                          delay=params["delay"])
+
+
+def upload_videos_fn():
+    upload_videos(paths=paths,
+                  iterations=iterations,
+                  delay=delay)
+
+
+def run_in_parallel(*fns):
+    proc = []
+    for fn in fns:
+        p = Process(target=fn)
+        p.start()
+        proc.append(p)
+    for p in proc:
+        p.join()
+
+
+print("Running download and upload functions in parallel")
+run_in_parallel(collect_camera_videos_fn, upload_videos_fn)
